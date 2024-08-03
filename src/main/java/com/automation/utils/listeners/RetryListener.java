1365007711bd3c11f9065eccd327bc5f5f999d68
mahe2421 <mahe.ame@gmail.com>
2024-08-03 22:11:47 +0530

    removed import org.testng.IRetryAnalyzer

--- src/main/java/com/automation/utils/listeners/RetryListener.java
+++ src/main/java/com/automation/utils/listeners/RetryListener.java
@@ -1,7 +1,6 @@
 package com.automation.utils.listeners;
 
 import org.testng.IAnnotationTransformer;
-import org.testng.IRetryAnalyzer;
 import org.testng.annotations.ITestAnnotation;
 
 import java.lang.reflect.Constructor;
